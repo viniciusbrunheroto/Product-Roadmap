e69a3721c469133bf4a61e17aed9161525e3289b
Vinicius Brunheroto <vinibrunheroto12@gmail.com>
2026-02-25 18:00:27 -0300

    testing

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,9 @@ import { clientEnv } from '@/env'
 import {createAuthClient} from 'better-auth/react'
 
 
+console.log("NEXT_PUBLIC_API_URL:", clientEnv.NEXT_PUBLIC_API_URL);
+
+
 export const authClient = createAuthClient({
     baseURL: clientEnv.NEXT_PUBLIC_API_URL
 })
