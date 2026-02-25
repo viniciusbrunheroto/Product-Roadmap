0ada406c22f3d4d69d0a779d18308c676eb9ce7e
Vinicius Brunheroto <vinibrunheroto12@gmail.com>
2026-02-25 18:36:04 -0300

    fix errors

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,11 @@ import { clientEnv } from '@/env'
 import {createAuthClient} from 'better-auth/react'
 
 
-console.log("NEXT_PUBLIC_API_URL:", clientEnv.NEXT_PUBLIC_API_URL);
+console.log("VERCEL_URL:", clientEnv.VERCEL_URL);
 
 
-export const authClient = createAuthClient()
+export const authClient = createAuthClient(
+    {
+        baseURL: clientEnv.VERCEL_URL,
+    }
+)
